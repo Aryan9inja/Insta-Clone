4c0385ceda5e870da91511f214b9264cb168f9f6
Aryan Singh Thakur <aryana123b456@gmail.com>
2025-07-14 23:25:03 +0530

    Setup constants for appwrite services

---
+++ src/constants/appwrite.ts
@@ -0,0 +1,12 @@
+export const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID!;
+export const BUCKET_ID = import.meta.env.VITE_APPWRITE_BUCKET_ID!;
+
+export const COLLECTION_USERS = "users";
+export const COLLECTION_POSTS = "posts";
+export const COLLECTION_LIKES = "likes";
+export const COLLECTION_FOLLOWERS = "followers";
+
+export const DEFAULT_PROFILE_IMAGE_ID = import.meta.env
+  .VITE_DEFAULT_PROFILE_IMAGE_ID!;
+
+export const REDIRECT_URL = import.meta.env.VITE_APPWRITE_EMAIL_VERIFY_REDIRECT!;
