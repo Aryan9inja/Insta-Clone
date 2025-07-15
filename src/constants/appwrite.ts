e2dcf7f6d9a91e989b3b2afa3a8c17280cd6bfe0
Aryan Singh Thakur <aryana123b456@gmail.com>
2025-07-16 00:05:02 +0530

    Add required appwrite credentials

--- src/constants/appwrite.ts
+++ src/constants/appwrite.ts
@@ -1,12 +1,14 @@
 export const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID!;
 export const BUCKET_ID = import.meta.env.VITE_APPWRITE_BUCKET_ID!;
 
-export const COLLECTION_USERS = "users";
-export const COLLECTION_POSTS = "posts";
-export const COLLECTION_LIKES = "likes";
-export const COLLECTION_FOLLOWERS = "followers";
+export const COLLECTION_USERS = import.meta.env.VITE_APPWRITE_COLLECTION_USERS;
+export const COLLECTION_POSTS = import.meta.env.VITE_APPWRITE_COLLECTION_POSTS;
+export const COLLECTION_LIKES = import.meta.env.VITE_APPWRITE_COLLECTION_LIKES;
+export const COLLECTION_FOLLOWERS = import.meta.env
+  .VITE_APPWRITE_COLLECTION_FOLLOWERS;
 
 export const DEFAULT_PROFILE_IMAGE_ID = import.meta.env
   .VITE_DEFAULT_PROFILE_IMAGE_ID!;
 
-export const REDIRECT_URL = import.meta.env.VITE_APPWRITE_EMAIL_VERIFY_REDIRECT!;
+export const REDIRECT_URL = import.meta.env
+  .VITE_APPWRITE_EMAIL_VERIFY_REDIRECT!;
