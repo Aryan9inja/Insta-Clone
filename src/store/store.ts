6390454c9751b17f0790f018461469331c7c0dfa
Aryan Singh Thakur <aryana123b456@gmail.com>
2025-07-15 18:48:17 +0530

    Add redux store

---
+++ src/store/store.ts
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import usersReducer from "./users.slice";
+
+const store = configureStore({
+  reducer: {
+    users: usersReducer,
+  },
+  devTools: import.meta.env.MODE !== "production",
+});
+
+export default store;
