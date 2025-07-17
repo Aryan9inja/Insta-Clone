7d5919fda689a64055b4f38d811f0d2750e9fadb
Aryan Singh Thakur <aryana123b456@gmail.com>
2025-07-17 22:56:21 +0530

    Add splash loader

---
+++ src/components/ui/splashScreen.tsx
@@ -0,0 +1,7 @@
+export default function SplashScreen() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-light-bg dark:bg-dark-bg">
+      <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-light-primary dark:border-dark-primary"></div>
+    </div>
+  );
+}
