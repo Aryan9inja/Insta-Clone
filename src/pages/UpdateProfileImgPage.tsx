2f93589af14c7c462a969fafd573226a4d9a9482
Aryan Singh Thakur <aryana123b456@gmail.com>
2025-07-22 01:41:25 +0530

    Add profile image update page

---
+++ src/pages/UpdateProfileImgPage.tsx
@@ -0,0 +1,11 @@
+import UpdateProfileImg from "../components/profile/updateProfileImg";
+
+const UpdateProfileImgPage = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-[var(--color-light-bg)] dark:bg-[var(--color-dark-bg)]">
+      <UpdateProfileImg />
+    </div>
+  );
+};
+
+export default UpdateProfileImgPage;
