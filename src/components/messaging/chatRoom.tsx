7d872d0a52585e1295c5af055c205a3e34c99d75
Aryan Singh Thakur <aryana123b456@gmail.com>
2025-07-31 14:33:37 +0530

    Remove harcoded value

--- src/components/messaging/chatRoom.tsx
+++ src/components/messaging/chatRoom.tsx
@@ -2,7 +2,7 @@ import { useEffect, useRef, useState } from "react";
 import { io, Socket } from "socket.io-client";
 import { getProfileImgUrl } from "../../services/users.services";
 
-const SOCKET_SERVER_URL = "http://localhost:3000";
+const SOCKET_SERVER_URL = import.meta.env.VITE_SOCKET_SERVER_URL;
 
 interface Props {
   userId: string;
